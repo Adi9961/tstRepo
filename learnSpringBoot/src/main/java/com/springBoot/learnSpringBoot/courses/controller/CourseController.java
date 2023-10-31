c3fe6a3cf7c5806a5b8d17087f1a8416fedc91b1
Aditya Gupta <adityaagupta9961@gmail.com>
2023-10-31 18:38:47 +0530

    Specific features changes

--- learnSpringBoot/src/main/java/com/springBoot/learnSpringBoot/courses/controller/CourseController.java
+++ learnSpringBoot/src/main/java/com/springBoot/learnSpringBoot/courses/controller/CourseController.java
@@ -37,7 +37,7 @@ public class CourseController {
 	public String startProject() {
 
 		System.out.println("Developer of the application is :Aditya Gupta");
-		System.out.println("For testing purpose");
+		System.out.println("For testing purpose for specefic feature.");
 		System.out.println("This application is under development by latestTech...");
 		return "Hi this is the intial stage of the spring project by latestTech and all the backend API dvelopment would be manged here..";
 	}
